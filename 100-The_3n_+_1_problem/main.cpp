7bfce0de8a267f920acc86a9f2145d03a0467fda
hugues.vincent.ropert@tetha.pw <hugues.vincent.ropert@tetha.pw>
2018-10-25 00:06:42 +0200

    p100 3n+1 pb

--- 100-The_3n_+_1_problem/main.cpp
+++ 100-The_3n_+_1_problem/main.cpp
@@ -1,73 +1,88 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <math.h>
-#include <algorithm>
-#include <time.h>
+#include <map>
 
-#include "header/utils.h"
 using namespace std;
-int min(int t[], int n){
-	int min = t[0];
-	for (int i=1 ;i<n; i++)
-		min = min < t[i] ? min : t[i];
-	return min;
+int f(int n) {
+	return n % 2 == 1 ? 3 * n + 1 : n / 2;
 }
-int max(int t[], int n){
-	int max = t[0];
-	for (int i=1 ;i<n; i++)
-		max = max > t[i] ? max : t[i];
-	return max;
+
+int f_k(int n, int k, int c[] ,int d[]) {
+	int a = floor(n/pow(2, k));
+	int b = n % int(pow(2, k));
+	cout << a << " " << b <<endl;
+	return a * pow(3, c[b]) + d[b];
 }
-int cycle(int i){
-	int n = 0;
-	if (i==0)
-		return 0;
-	n++;
-	while(i != 1){
-		n ++;
-		i = i % 2 == 0 ? i/2 : 3*i+1;	
-	}
-	return n;
+
+void cycle_graph(int n, map<int, int> &foo) {
+	bool loop = true;
+	int cycle_length = 0;
+	int tmp_n = n;
+	while(loop){
+		foo[n] =  foo.find(n) == foo.end() ? 0 : foo[n] + 1;
+		if (n==1)
+		 	loop = false;
+		n = f(n);
+	 	cycle_length++;
+	}	
 }
-int cycle(int i, int t[]){
-	int n = 0, i_0=i;
-	if (i==0)
-		return 0;
-	while(i != 0){
-		if (i < i_0){
-			n += t[i];
-			i = 0;
+void cycle(int n, map<int, int> &cycle_lengths) {
+	bool loop = true;
+	int cycle_length = 0;
+	int tmp_n = n;
+	while(loop){
+		// cout << n << " ";
+	auto cur = cycle_lengths.find(n);
+	if (cur == cycle_lengths.end())
+	{
+	 	cycle_lengths[n] = -cycle_length;
+			if (n==1)
+			 	loop = false;
+		 	tmp_n = n;
+			n = f(n);
+		 	cycle_length++;
 		}
-		else{
-			n ++;
-			i = i % 2 == 0 ? i/2 : 3*i+1;	
+		else {
+			loop = false;
+			cycle_length += cur->second;
 		}
 	}
-	return n;
+	for(auto &it : cycle_lengths) {
+		if (it.second <=0 ) 
+			it.second += cycle_length;
+	}
+	// cout << endl;
 }
-void print(int t[], int n){
-	for(int i=0; i<n; i++){
-		cout << i << ": " << t[i] << "\n";
+int main(int argc, char *argv[]) {
+	int k = 6;
+	
+	int i, j;
+	int max_cycle;
+	map<int, int> cycle_lengths, occurence;
+	
+	for(int l = 1; l <= 10000 ; l++){
+		cycle_graph(l, occurence);
+		cycle(l, cycle_lengths);
 	}
-}
-int main(int argc, char *argv[]){
-	char *p;
-	int i = 1, j = 110000, t[j], b[j];
-	// n = strtol(argv[1], &p, 10);
-    clock_t tStart2 = clock();
-	for(int l=0; l<j; l++){
-		b[l] = cycle(l) ;
+	ofstream myfile;
+	myfile.open ("cache");
+	myfile << "{";
+	for(auto kv : occurence) {
+		if ((kv.second > 11 && kv.first > 4000 )|| (kv.second > 5 && kv.first > 30000))
+		myfile << "{" << kv.first << ", " << cycle_lengths[kv.first] << "},";
+		
 	}
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart2)/CLOCKS_PER_SEC);
-    clock_t tStart = clock();
-	t[0] = 0; 
-	t[1] = 1; 
-	for(int l=2; l<j; l++){
-		t[l] = cycle(l, t);
-	}
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
-	// cout << cycle(1);
+	myfile << "}\n";
+	myfile.close();
+
+	// while(scanf("%d%d", &i, &j) != EOF)
+	// {	max_cycle = 0;
+	// 	for(int l=i; l <= j; l ++){
+	// 		cycle(l, cycle_lengths);
+	// 		max_cycle = max_cycle > cycle_lengths[l] ? max_cycle : cycle_lengths[l];
+	// 	}
+	// 	cout << i << " " << j << " " << max_cycle << "\n"; 
+	// }
 	return 0;
 }
-
