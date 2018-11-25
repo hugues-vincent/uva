db707e4e69d07d4abc12c617aa69a60a0db59eec
hugues.vincent.ropert@tetha.pw <hugues.vincent.ropert@tetha.pw>
2018-11-25 17:09:24 +0100

    [11935] optimize

--- 11935-Through_the_desert/main.cpp
+++ 11935-Through_the_desert/main.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
-#include <cstring>
 #include <queue>
 
 using namespace std;
-
+double max(double a, double b) { return a > b ? a : b; }
 int main() {
     char e[17];
-    priority_queue<double> pq;
     int km, last_km, fuel_consumption, leak;
-    double fuel;
-    fuel = fuel_consumption = km = last_km = leak = 0;
+    double fuel, max_fuel;
+    max_fuel = fuel = fuel_consumption = km = last_km = leak = 0;
     while (scanf("%d %s", &km, e) != EOF) {
         fuel += leak * (km - last_km) + fuel_consumption * (double)(km - last_km) / 100; 
         last_km = km;
-        if (strcmp(e, "Fuel") == 0) { if(scanf("%s %d", e, &fuel_consumption)){} }
-        else if(strcmp(e, "Leak") == 0) leak++;
-        else if(strcmp(e, "Gas") == 0){
+        if (e[0] == 'F') { if(scanf("%s %d", e, &fuel_consumption)){} }
+        else if(e[0] == 'L') leak++;
+        else if(e[1] == 'a'){
             if(scanf("%s", e)){}
-            pq.push(fuel);
+            max_fuel = max(max_fuel, fuel);
             fuel = 0;
         }
-        else if(strcmp(e, "Mechanic") == 0) leak = 0;
-        else if(strcmp(e, "Goal") == 0){
-            pq.push(fuel);
-            printf("%0.3f\n", pq.top());
-            pq = priority_queue<double>();
-            fuel = fuel_consumption = km = last_km = leak = 0;
+        else if(e[0] == 'M') leak = 0;
+        else {
+            printf("%0.3f\n", max( max_fuel, fuel));
+            max_fuel = fuel = fuel_consumption = km = last_km = leak = 0;
         }
     }
     return 0;
