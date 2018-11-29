597bc3536916fcd836a83d24b1aa836fe1de06a6
hugues.vincent.ropert@tetha.pw <hugues.vincent.ropert@tetha.pw>
2018-11-29 13:55:38 +0100

    [11450] recursive local

--- 11450-Wedding_Shopping/main.cpp
+++ 11450-Wedding_Shopping/main.cpp
@@ -7,16 +7,27 @@ typedef vector<int> vi;
 int N, M, C, K, best;
 vi garment[20];
 
-void recursive(int c, int m){
+void recursive_global(int c, int m){
     if (m < 0) return;
     if (c == C && best < M - m) { 
         best = M - m; 
         return;
     }
     for (int k = 0; k < garment[c].size(); ++k)
-        recursive(c + 1, m - garment[c][k]);
+        recursive_global(c + 1, m - garment[c][k]);
 }
 
+int max(int a, int b) { return a >=b ? a: b; }
+int recursive_local(int c, int m) {
+    if (m < 0) return -(1<<25);
+    int local_max = -1;
+    if(c == C) return M - m;
+    for (int k = 0; k < garment[c].size(); ++k)
+    {
+        local_max = max(local_max, recursive_local(c +1, m - garment[c][k]));
+    }
+    return local_max;
+}
 int main() { 
     int tmp;
     if(scanf("%d", &N)){}
@@ -31,9 +42,7 @@ int main() {
 
             }
         }
-        best = -1;
-        recursive(0, M);
-        printf("\n");
+        best = recursive_local(0, M);
         best == -1 ? printf("no solution\n") : printf("%d\n", best);
     }
 
